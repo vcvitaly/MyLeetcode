4d65debe0e150d709ea2611d35f8c9f5ae0d1c50
Vitaly Chura <vcvitaly@yahoo.com>
2018-11-24 18:31:24 +0200

    LongestUnivaluePath - ip

---
+++ src/main/java/com/github/vcvitaly/_687/LongestUnivaluePath.java
@@ -0,0 +1,7 @@
+package com.github.vcvitaly._687;
+
+public class LongestUnivaluePath {
+    public int longestUnivaluePath(TreeNode root) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/github/vcvitaly/_687/LongestUnivaluePathTest.java
@@ -0,0 +1,7 @@
+package com.github.vcvitaly._687;
+
+import static org.junit.Assert.*;
+
+public class LongestUnivaluePathTest {
+
+}
