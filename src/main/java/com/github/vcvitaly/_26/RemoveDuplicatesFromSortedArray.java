d12790041aa75e2b8b4c41cc71695020da168fc1
vitalii-chura-axon <vchura@axon.dev>
2022-08-14 15:58:42 +0300

    Solve RemoveDuplicatesFromSortedArray

--- src/main/java/com/github/vcvitaly/_26/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/github/vcvitaly/_26/RemoveDuplicatesFromSortedArray.java
@@ -7,21 +7,40 @@ package com.github.vcvitaly._26;
  */
 public class RemoveDuplicatesFromSortedArray {
 
-    public int removeDuplicates(int[] nums) {
+    public int removeDuplicatesSlow(int[] nums) {
         if (nums.length <= 1) {
             return nums.length;
         }
-        int k = 0;
-        for (int i = 0; i < nums.length || nums[i] == -1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
+        int k = nums.length;
+        for (int i = 0; i < k; i++) {
+            for (int j = i + 1; j < k ; j++) {
                 if (nums[i] == nums[j]) {
-                    for (int l = j + 1; l < nums.length; l++) {
+                    // shifting to left by one position
+                    for (int l = j + 1; l < k; l++) {
                         nums[l-1] = nums[l];
                     }
+                    nums[k-1] = -1;
+                    k--;
+                    j--; // because all elements were shifted to left, if we do not do that we might skip an element
                 }
             }
-            k++;
         }
         return k;
     }
+
+    /*
+    This solution is not mine
+    public int removeDuplicates(int[] nums) {
+
+        int count=1;
+
+        for(int i=1, j=0; i < nums.length; i++){
+            if(nums[j] != nums[i]){
+                count++;
+                nums[++j] = nums[i];
+
+            }
+        }
+        return count;
+    }*/
 }
