e2482109033971d239911d0873d73da2e07af64d
vitalii-chura-axon <vchura@axon.dev>
2022-08-20 19:46:16 +0300

    Solve RemoveDuplicatesFromSortedList

---
+++ src/main/java/com/github/vcvitaly/_83/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,28 @@
+package com.github.vcvitaly._83;
+
+import java.util.List;
+
+/**
+ * RemoveDuplicatesFromSortedList.
+ *
+ * @author Vitalii Chura
+ */
+public class RemoveDuplicatesFromSortedList {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode originalHead = head;
+        while (head.next != null) {
+            if (head.val == head.next.val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+
+        return originalHead;
+    }
+}
---
+++ src/test/java/com/github/vcvitaly/_83/RemoveDuplicatesFromSortedListTest.java
@@ -0,0 +1,61 @@
+package com.github.vcvitaly._83;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * RemoveDuplicatesFromSortedListTest.
+ *
+ * @author Vitalii Chura
+ */
+class RemoveDuplicatesFromSortedListTest {
+
+    private RemoveDuplicatesFromSortedList duplicateRemover = new RemoveDuplicatesFromSortedList();
+
+    @ParameterizedTest
+    @MethodSource("params")
+    void removesDuplicatesFromALinkedList(Param p) {
+        System.out.println(p);
+
+
+        List<Integer> result = new ArrayList<>();
+        ListNode resultHead = duplicateRemover.deleteDuplicates(p.head);
+        while (resultHead != null) {
+            result.add(resultHead.val);
+            resultHead = resultHead.next;
+        }
+        assertThat(result).containsExactlyElementsOf(p.result);
+    }
+
+    static Stream<Param> params() {
+        return Stream.of(
+                Param.of(new ListNode(1,
+                                new ListNode(1,
+                                        new ListNode(2))),
+                        List.of(1, 2)),
+                Param.of(new ListNode(1,
+                                new ListNode(1,
+                                        new ListNode(2,
+                                                new ListNode(3,
+                                                        new ListNode(3))))),
+                        List.of(1, 2, 3)),
+                Param.of(new ListNode(1,
+                                new ListNode(1)),
+                        List.of(1))
+        );
+    }
+
+    @Data
+    @AllArgsConstructor(staticName = "of")
+    private static class Param {
+        public ListNode head;
+        private List<Integer> result;
+    }
+}
