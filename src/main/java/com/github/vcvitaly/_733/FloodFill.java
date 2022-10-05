331783326a41f5da5f18192bbb2fdac3f4987644
vitalii-chura-axon <vchura@axon.dev>
2022-10-05 10:27:43 +0300

    Improve FloodFill

--- src/main/java/com/github/vcvitaly/_733/FloodFill.java
+++ src/main/java/com/github/vcvitaly/_733/FloodFill.java
@@ -13,15 +13,16 @@ public class FloodFill {
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int origColor = image[sr][sc];
-        boolean[][] visited = new boolean[image.length][image[0].length];
+        if (origColor == color) {
+            return image;
+        }
         Stack<CellIndex> stack = new Stack<>();
         stack.push(new CellIndex(sr, sc));
         while (!stack.isEmpty()) {
             CellIndex currentCell = stack.pop();
             image[currentCell.i][currentCell.j] = color;
-            visited[currentCell.i][currentCell.j] = true;
             for (CellIndex unvisitedNeighborCoordinate :
-                    unvisitedNeighborCoordinates(image, visited, currentCell.i, currentCell.j, origColor)) {
+                    unvisitedNeighborCoordinates(image, currentCell.i, currentCell.j, origColor)) {
                 stack.push(unvisitedNeighborCoordinate);
             }
         }
@@ -29,19 +30,19 @@ public class FloodFill {
     }
 
     private List<CellIndex> unvisitedNeighborCoordinates(
-            int[][] image, boolean[][] visited, int i, int j, int color) {
+            int[][] image, int i, int j, int origColor) {
         List<CellIndex> neighborCoordinates = new ArrayList<>();
 
-        if (i-1 >= 0 && !visited[i-1][j] && image[i-1][j] == color) {
+        if (i-1 >= 0 && image[i-1][j] == origColor) {
             neighborCoordinates.add(new CellIndex(i-1,j));
         }
-        if (j+1 < visited[0].length && !visited[i][j+1] && image[i][j+1] == color) {
+        if (j+1 < image[0].length && image[i][j+1] == origColor) {
             neighborCoordinates.add(new CellIndex(i, j+1));
         }
-        if (i+1 < visited.length && !visited[i+1][j] && image[i+1][j] == color) {
+        if (i+1 < image.length && image[i+1][j] == origColor) {
             neighborCoordinates.add(new CellIndex(i+1,j));
         }
-        if (j-1 >= 0 && !visited[i][j-1] && image[i][j-1] == color) {
+        if (j-1 >= 0 && image[i][j-1] == origColor) {
             neighborCoordinates.add(new CellIndex(i, j-1));
         }
 
