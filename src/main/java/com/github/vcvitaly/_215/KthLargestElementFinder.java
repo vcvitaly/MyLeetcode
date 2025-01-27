5b6ed3c6edf5e662b07efc031fa06a960966f15d
Vitalii Chura <vitalii.chura@ge.com>
2025-01-27 04:01:02 +0200

    215 - n*k solution WIP

--- src/main/java/com/github/vcvitaly/_215/KthLargestElementFinder.java
+++ src/main/java/com/github/vcvitaly/_215/KthLargestElementFinder.java
@@ -1,18 +1,28 @@
 package com.github.vcvitaly._215;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class KthLargestElementFinder {
 
     public int findKthLargest(int[] nums, int k) {
-        final PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        for (int i = 0; i < nums.length; i++) {
-            pq.add(nums[i]);
+        int largest = Integer.MIN_VALUE;
+        int prevLargest = Integer.MAX_VALUE;
+        final Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int i = 0; i < k; i++) {
+            largest = Integer.MIN_VALUE;
+            for (int j = 0; j < nums.length; j++) {
+                final int cur = nums[j];
+                if (cur > largest && cur <= prevLargest &&
+                        !map.getOrDefault(cur, Set.of()).contains(j)) {
+                    largest = cur;
+                    map.computeIfAbsent(cur, key -> new HashSet<>()).add(j);
+                }
+            }
+            prevLargest = largest;
         }
-        for (int i = 1; i < k; i++) {
-            pq.poll();
-        }
-        return pq.peek();
+        return largest;
     }
 }
