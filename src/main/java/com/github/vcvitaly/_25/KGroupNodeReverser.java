d57ef64a6c3fa13dac0da44d9c729d58dc73af3e
Vitalii Chura <vcvitaly@yahoo.com>
2025-02-10 08:12:59 +0100

    Draft 25

---
+++ src/main/java/com/github/vcvitaly/_25/KGroupNodeReverser.java
@@ -0,0 +1,10 @@
+package com.github.vcvitaly._25;
+
+import com.github.vcvitaly.common.ListNode;
+
+public class KGroupNodeReverser {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        return null;
+    }
+}
