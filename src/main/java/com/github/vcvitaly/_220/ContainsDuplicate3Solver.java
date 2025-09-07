ac1159232b87fc9241f3940f566e05618742f06c
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-09-07 19:01:52 +0200

    220 - draft

---
+++ src/main/java/com/github/vcvitaly/_220/ContainsDuplicate3Solver.java
@@ -0,0 +1,8 @@
+package com.github.vcvitaly._220;
+
+public class ContainsDuplicate3Solver {
+
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+        return false;
+    }
+}
---
+++ src/test/java/com/github/vcvitaly/_220/ContainsDuplicate3SolverTest.java
@@ -0,0 +1,24 @@
+package com.github.vcvitaly._220;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ContainsDuplicate3SolverTest {
+
+    private final ContainsDuplicate3Solver solver = new ContainsDuplicate3Solver();
+
+    @Test
+    void test1() {
+        assertThat(
+                solver.containsNearbyAlmostDuplicate(new int[]{1,2,3,1}, 3, 0)
+        ).isTrue();
+    }
+
+    @Test
+    void test2() {
+        assertThat(
+                solver.containsNearbyAlmostDuplicate(new int[]{1,5,9,1,5,9}, 2, 3)
+        ).isTrue();
+    }
+}
