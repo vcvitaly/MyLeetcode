d5f845377436c07e84d76cbaf806c304bbcdbbd5
Vitalii Chura <vcvitaly@yahoo.com>
2025-09-07 06:29:00 +0200

    15 - solve, but too slow

--- src/main/java/com/github/vcvitaly/_15/ThreeSum.java
+++ src/main/java/com/github/vcvitaly/_15/ThreeSum.java
@@ -1,14 +1,6 @@
 package com.github.vcvitaly._15;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 /**
  * ThreeSum.
@@ -18,11 +10,18 @@ import java.util.Set;
 public class ThreeSum {
 
     public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
         Set<List<Integer>> result = new HashSet<>();
         // nums.length -1 to account for + 1 in the code
         for (int i = 0; i < nums.length - 1; i++) {
+            if (i >= 1 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if (nums[i] > 0) {
+                break;
+            }
             int difference = -nums[i];
-            List<int[]> pairs = twoSum(nums, i + 1, difference);
+            Set<int[]> pairs = twoSum(nums, i + 1, difference);
             for (int[] pair : pairs) {
                 List<Integer> triple = new ArrayList<>(List.of(nums[i], pair[0], pair[1]));
                 triple.sort(Comparator.naturalOrder());
@@ -32,16 +31,21 @@ public class ThreeSum {
         return new ArrayList<>(result);
     }
 
-    private List<int[]> twoSum(int[] nums, int startIndex, int target) {
-        List<int[]> pairs = new ArrayList<>();
+    private Set<int[]> twoSum(int[] nums, int startIndex, int target) {
+        Set<int[]> pairs = new HashSet<>();
         Set<Integer> set = new HashSet<>();
+        Set<Integer> pairFoundFor = new HashSet<>();
         for (int i = startIndex; i < nums.length; i++) {
+            if (i > startIndex && pairFoundFor.contains(nums[i])) {
+                continue;
+            }
             int difference = target - nums[i];
             if (set.contains(difference)) {
                 int[] pair = new int[2];
                 pair[0] = difference;
                 pair[1] = nums[i];
                 pairs.add(pair);
+                pairFoundFor.add(difference);
             }
             set.add(nums[i]);
         }
