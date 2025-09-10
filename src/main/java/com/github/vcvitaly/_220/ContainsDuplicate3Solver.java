f82c223fcb639094305606ec3962e3c68319eb85
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-09-10 03:35:09 +0200

    220 - solve, but very slowly

--- src/main/java/com/github/vcvitaly/_220/ContainsDuplicate3Solver.java
+++ src/main/java/com/github/vcvitaly/_220/ContainsDuplicate3Solver.java
@@ -1,30 +1,72 @@
 package com.github.vcvitaly._220;
 
-import java.util.NavigableSet;
-import java.util.TreeSet;
+import java.util.*;
 
 public class ContainsDuplicate3Solver {
 
     public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
-        final NavigableSet<Integer> set = new TreeSet<>();
-        for (int i = 1; i <= indexDiff; i++) {
-            set.add(nums[i]);
+        final TreeList treeList = new TreeList();
+        for (int i = 1; i <= Math.min(indexDiff, nums.length - 1); i++) {
+            treeList.add(nums[i]);
         }
-        for (int i = 0; i < (nums.length - indexDiff); i++) {
+        int i = 0;
+        do {
             final int num = nums[i];
-            final Integer floor = set.floor(num);
-            final Integer ceiling = set.ceiling(num);
+            final Integer floor = treeList.floor(num);
+            final Integer ceiling = treeList.ceiling(num);
             if (floor != null && withingRange(num, floor, valueDiff) ||
                     ceiling != null && withingRange(num, ceiling, valueDiff)) {
                 return true;
             }
-            set.removeFirst();
-            set.add(nums[indexDiff + i + 1]);
-        }
+            treeList.removeFirst();
+            i++;
+            if (indexDiff + i < nums.length) {
+                treeList.add(nums[indexDiff + i]);
+            }
+        } while (i < (nums.length - indexDiff) || !treeList.isEmpty());
         return false;
     }
 
     private boolean withingRange(int num, int otherNum, int valueDiff) {
         return Math.abs(num - otherNum) <= valueDiff;
+    }
+
+    private record TreeList(SequencedMap<Integer, Integer> bag, NavigableSet<Integer> tree) {
+        TreeList() {
+            this(new LinkedHashMap<>(), new TreeSet<>());
+        }
+
+        public void add(Integer i) {
+            bag.merge(i, 1, Integer::sum);
+            tree.add(i);
+        }
+
+        public void removeFirst() {
+            final Map.Entry<Integer, Integer> first = bag.firstEntry();
+            if (first != null) {
+                final Integer key = first.getKey();
+                final Integer val = first.getValue();
+                if (val == 1) {
+                    bag.remove(key);
+                    tree.remove(key);
+                } else if (val > 1) {
+                    bag.putFirst(key, val - 1);
+                } else {
+                    throw new IllegalStateException("Key should be >= 1 but was: " + key);
+                }
+            }
+        }
+
+        public Integer floor(int i) {
+            return tree.floor(i);
+        }
+
+        public Integer ceiling(Integer i) {
+            return tree.ceiling(i);
+        }
+
+        public boolean isEmpty() {
+            return bag.isEmpty();
+        }
     }
 }
--- src/test/java/com/github/vcvitaly/_220/ContainsDuplicate3SolverTest.java
+++ src/test/java/com/github/vcvitaly/_220/ContainsDuplicate3SolverTest.java
@@ -23,9 +23,37 @@ class ContainsDuplicate3SolverTest {
     }
 
     @Test
+    void test2_1() {
+        assertThat(
+                solver.containsNearbyAlmostDuplicate(new int[]{1,5,9,1,5,9}, 3, 0)
+        ).isTrue();
+    }
+
+    @Test
     void test3() {
         assertThat(
                 solver.containsNearbyAlmostDuplicate(new int[]{1,3}, 1, 2)
+        ).isTrue();
+    }
+
+    @Test
+    void test4() {
+        assertThat(
+                solver.containsNearbyAlmostDuplicate(new int[]{-2,3}, 2, 5)
+        ).isTrue();
+    }
+
+    @Test
+    void test5() {
+        assertThat(
+                solver.containsNearbyAlmostDuplicate(new int[]{1,2,2,3,4,5}, 3, 0)
+        ).isTrue();
+    }
+
+    @Test
+    void test6() {
+        assertThat(
+                solver.containsNearbyAlmostDuplicate(new int[]{7,1,3}, 2, 3)
         ).isTrue();
     }
 }
