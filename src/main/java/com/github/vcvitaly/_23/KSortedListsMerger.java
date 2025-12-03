74fc36eefcfbd6477a8d84394185bbe1b667bc09
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-12-03 19:01:24 +0100

    23 - draft

---
+++ src/main/java/com/github/vcvitaly/_23/KSortedListsMerger.java
@@ -0,0 +1,10 @@
+package com.github.vcvitaly._23;
+
+import com.github.vcvitaly.common.ListNode;
+
+public class KSortedListsMerger {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        return null;
+    }
+}
