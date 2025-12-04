738127d2f8e2a4a7b9e4f67e0601d1915d0b431c
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-12-04 18:24:25 +0100

    6 - slow solution

--- src/main/java/com/github/vcvitaly/_6/ZigZagConverter.java
+++ src/main/java/com/github/vcvitaly/_6/ZigZagConverter.java
@@ -1,8 +1,41 @@
 package com.github.vcvitaly._6;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class ZigZagConverter {
 
     public String convert(String s, int numRows) {
-        return null;
+        if (numRows == 1) {
+            return s;
+        }
+
+        final List<ArrayList<Character>> rows = IntStream.rangeClosed(1, numRows)
+                .mapToObj(i -> new ArrayList<Character>())
+                .toList();
+
+        boolean increasing = true;
+        int rowNum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            final char c = s.charAt(i);
+            rows.get(rowNum).add(c);
+            if (increasing) {
+                rowNum++;
+                if (rowNum == numRows - 1) {
+                    increasing = false;
+                }
+            } else {
+                rowNum--;
+                if (rowNum == 0) {
+                    increasing = true;
+                }
+            }
+        }
+
+        return rows.stream()
+                .map(r -> r.stream().map(String::valueOf).collect(Collectors.joining("")))
+                .collect(Collectors.joining(""));
     }
 }
