1c4fed799cc42a265e3747a540eb3cb15ffbcc68
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-12-04 17:34:36 +0100

    22 - draft

---
+++ src/main/java/com/github/vcvitaly/_22/ParenthesisGenerator.java
@@ -0,0 +1,10 @@
+package com.github.vcvitaly._22;
+
+import java.util.List;
+
+public class ParenthesisGenerator {
+
+    public List<String> generateParenthesis(int n) {
+        return null;
+    }
+}
---
+++ src/test/java/com/github/vcvitaly/_22/ParenthesisGeneratorTest.java
@@ -0,0 +1,24 @@
+package com.github.vcvitaly._22;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ParenthesisGeneratorTest {
+
+    private final ParenthesisGenerator generator = new ParenthesisGenerator();
+
+    @Test
+    void test1() {
+        assertThat(generator.generateParenthesis(1))
+                .isEqualTo(List.of("()"));
+    }
+
+    @Test
+    void test2() {
+        assertThat(generator.generateParenthesis(3))
+                .isEqualTo(List.of("((()))","(()())","(())()","()(())","()()()"));
+    }
+}
