d4d06d0a31671df48b97c8b69eab60bc1e9f61cb
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-12-05 21:15:52 +0100

    118 - draft

---
+++ src/main/java/com/github/vcvitaly/_118/PascalTriangleSolver.java
@@ -0,0 +1,10 @@
+package com.github.vcvitaly._118;
+
+import java.util.List;
+
+public class PascalTriangleSolver {
+
+    public List<List<Integer>> generate(int numRows) {
+        return List.of();
+    }
+}
---
+++ src/test/java/com/github/vcvitaly/_118/PascalTriangleSolverTest.java
@@ -0,0 +1,30 @@
+package com.github.vcvitaly._118;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PascalTriangleSolverTest {
+
+    private final PascalTriangleSolver solver = new PascalTriangleSolver();
+
+    @Test
+    void test1() {
+        assertThat(solver.generate(1))
+                .containsExactly(List.of(1));
+    }
+
+    @Test
+    void test2() {
+        assertThat(solver.generate(2))
+                .containsExactly(List.of(1), List.of(1,1));
+    }
+
+    @Test
+    void test5() {
+        assertThat(solver.generate(2))
+                .containsExactly(List.of(1), List.of(1,1), List.of(1,2,1), List.of(1,3,3,1), List.of(1,4,6,4,1));
+    }
+}
