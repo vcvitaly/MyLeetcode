2c76b6d182c3c9dc451ca587a7e9a8c5a30c5242
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-12-05 21:01:29 +0100

    135 - WIP

--- src/main/java/com/github/vcvitaly/_135/CandyDistributionSolver.java
+++ src/main/java/com/github/vcvitaly/_135/CandyDistributionSolver.java
@@ -3,6 +3,10 @@ package com.github.vcvitaly._135;
 public class CandyDistributionSolver {
 
     public int candy(int[] ratings) {
+        if (ratings.length == 1) {
+            return 1;
+        }
+
         return -1;
     }
 }
--- src/test/java/com/github/vcvitaly/_135/CandyDistributionSolverTest.java
+++ src/test/java/com/github/vcvitaly/_135/CandyDistributionSolverTest.java
@@ -19,4 +19,34 @@ class CandyDistributionSolverTest {
         assertThat(solver.candy(new int[] {1,2,2}))
                 .isEqualTo(4);
     }
+
+    @Test
+    void test3_1() {
+        assertThat(solver.candy(new int[] {1,2,3}))
+                .isEqualTo(6);
+    }
+
+    @Test
+    void test3_2() {
+        assertThat(solver.candy(new int[] {3,2,1}))
+                .isEqualTo(6);
+    }
+
+    @Test
+    void test4() {
+        assertThat(solver.candy(new int[] {0,0,0}))
+                .isEqualTo(3);
+    }
+
+    @Test
+    void test5() {
+        assertThat(solver.candy(new int[] {1,2,0}))
+                .isEqualTo(4);
+    }
+
+    @Test
+    void test6() {
+        assertThat(solver.candy(new int[] {2,1,0}))
+                .isEqualTo(6);
+    }
 }
