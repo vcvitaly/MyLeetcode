8ad384f4345b8455372b82429259b574c7555776
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-12-06 10:59:11 +0100

    91 - draft

---
+++ src/main/java/com/github/vcvitaly/_91/DecodeWayCounter.java
@@ -0,0 +1,8 @@
+package com.github.vcvitaly._91;
+
+public class DecodeWayCounter {
+
+    public int numDecodings(String s) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/github/vcvitaly/_91/DecodeWayCounterTest.java
@@ -0,0 +1,25 @@
+package com.github.vcvitaly._91;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DecodeWayCounterTest {
+
+    private final DecodeWayCounter counter = new DecodeWayCounter();
+
+    @Test
+    void test1() {
+        assertThat(counter.numDecodings("12")).isEqualTo(2);
+    }
+
+    @Test
+    void test2() {
+        assertThat(counter.numDecodings("226")).isEqualTo(3);
+    }
+
+    @Test
+    void test3() {
+        assertThat(counter.numDecodings("06")).isEqualTo(0);
+    }
+}
