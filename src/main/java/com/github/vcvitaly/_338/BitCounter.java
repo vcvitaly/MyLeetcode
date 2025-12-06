78b937bbd6b476dd5d8104e2c6a2ddb9fbd8abc6
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-12-06 10:20:27 +0100

    338 - draft

---
+++ src/main/java/com/github/vcvitaly/_338/BitCounter.java
@@ -0,0 +1,8 @@
+package com.github.vcvitaly._338;
+
+public class BitCounter {
+
+    public int[] countBits(int n) {
+        return null;
+    }
+}
---
+++ src/test/java/com/github/vcvitaly/_338/BitCounterTest.java
@@ -0,0 +1,28 @@
+package com.github.vcvitaly._338;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BitCounterTest {
+
+    private BitCounter bitCounter = new BitCounter();
+
+    @Test
+    void test1() {
+        assertThat(bitCounter.countBits(2))
+                .containsExactly(0,1,1);
+    }
+
+    @Test
+    void test2() {
+        assertThat(bitCounter.countBits(5))
+                .containsExactly(0,1,1,2,1,2);
+    }
+
+    @Test
+    void test3() {
+        assertThat(bitCounter.countBits(0))
+                .containsExactly(0);
+    }
+}
