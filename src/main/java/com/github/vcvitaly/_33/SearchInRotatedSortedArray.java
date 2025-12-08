260c7a4151a47711c7b42e4c34bdc5a6fb751fee
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-12-08 05:07:46 +0100

    33 - WIP

--- src/main/java/com/github/vcvitaly/_33/SearchInRotatedSortedArray.java
+++ src/main/java/com/github/vcvitaly/_33/SearchInRotatedSortedArray.java
@@ -8,8 +8,11 @@ package com.github.vcvitaly._33;
 public class SearchInRotatedSortedArray {
 
     public int search(int[] nums, int target) {
-        if (nums.length == 1) {
-            return nums[0] == target ? 0 : -1;
+        if (nums.length <= 2) {
+            if (nums[0] == target) {
+                return 0;
+            }
+            return nums.length == 2 && nums[1] == target ? 1 : -1;
         }
 
         int k = 0;
@@ -18,15 +21,55 @@ public class SearchInRotatedSortedArray {
 
         while (left <= right) {
             int mid = (right - left) / 2 + left;
-            if (nums[mid] < nums[nums.length - 1]) {
-                right = mid - 1;
-                mid = (right - left) / 2 + left;
-            } else if (nums[mid] > nums[nums.length - 1]) {
+            int leftIndex = leftIndex(nums, mid);
+            if (nums[leftIndex] > nums[mid]) {
+                k = mid;
+                break;
+            }
+            if (nums[mid] > nums[0]) {
                 left = mid + 1;
-                mid = (right - left) / 2 + left;
+            } else {
+                right = mid - 1;
             }
         }
 
+        boolean targetBeforePivot = target <= nums[leftIndex(nums, k)];
+        if (target >= nums[k] && targetBeforePivot) {
+            if (target >= nums[0] && targetBeforePivot) {
+                left = 0;
+                right = leftIndex(nums, k);
+            } else {
+                left = k;
+                right = nums.length - 1;
+            }
+            while (left < right) {
+                int mid = (right - left) / 2 + left;
+                if (target == nums[mid]) {
+                    return mid;
+                }
+                if (nums[mid] > target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return nums[left] == target ? left : -1;
+        }
+
         return -1;
+    }
+
+    private int rightIndex(int[] nums, int i) {
+        if (i < nums.length - 1) {
+            return i + 1;
+        }
+        return 0;
+    }
+
+    private int leftIndex(int[] nums, int i) {
+        if (i == 0) {
+            return nums.length - 1;
+        }
+        return i - 1;
     }
 }
--- src/test/java/com/github/vcvitaly/_33/SearchInRotatedSortedArrayTest.java
+++ src/test/java/com/github/vcvitaly/_33/SearchInRotatedSortedArrayTest.java
@@ -30,4 +30,52 @@ class SearchInRotatedSortedArrayTest {
         int targetIndex = search.search(new int[] {1}, 0);
         assertThat(targetIndex).isEqualTo(-1);
     }
+
+    @Test
+    void test4() {
+        int targetIndex = search.search(new int[] {1,2,3}, 3);
+        assertThat(targetIndex).isEqualTo(2);
+    }
+
+    @Test
+    void test5() {
+        int targetIndex = search.search(new int[] {1,3}, 1);
+        assertThat(targetIndex).isEqualTo(0);
+    }
+
+    @Test
+    void test6() {
+        int targetIndex = search.search(new int[] {1,3}, 2);
+        assertThat(targetIndex).isEqualTo(-1);
+    }
+
+    @Test
+    void test7() {
+        int targetIndex = search.search(new int[] {3,1}, 1);
+        assertThat(targetIndex).isEqualTo(1);
+    }
+
+    @Test
+    void test8() {
+        int targetIndex = search.search(new int[] {1,3,5}, 3);
+        assertThat(targetIndex).isEqualTo(1);
+    }
+
+    @Test
+    void test9() {
+        int targetIndex = search.search(new int[] {4,5,1,2,3}, 1);
+        assertThat(targetIndex).isEqualTo(2);
+    }
+
+    @Test
+    void test10() {
+        int targetIndex = search.search(new int[] {3,1,2}, 2);
+        assertThat(targetIndex).isEqualTo(2);
+    }
+
+    @Test
+    void test11() {
+        int targetIndex = search.search(new int[] {5,1,2,3,4}, 4);
+        assertThat(targetIndex).isEqualTo(4);
+    }
 }
