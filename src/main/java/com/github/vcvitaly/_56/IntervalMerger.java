74f5cb8814fcc25d21856afb8f599d3fa6e83755
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-12-08 05:20:54 +0100

    56 - draft

---
+++ src/main/java/com/github/vcvitaly/_56/IntervalMerger.java
@@ -0,0 +1,8 @@
+package com.github.vcvitaly._56;
+
+public class IntervalMerger {
+
+    public int[][] merge(int[][] intervals) {
+        return null;
+    }
+}
---
+++ src/test/java/com/github/vcvitaly/_56/IntervalMergerTest.java
@@ -0,0 +1,34 @@
+package com.github.vcvitaly._56;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class IntervalMergerTest {
+
+    private final IntervalMerger intervalMerger = new IntervalMerger();
+
+    @Test
+    void test1() {
+        assertThat(intervalMerger.merge(new int[][] {{1,3},{2,6},{8,10},{15,18}}))
+                .isDeepEqualTo(new int[][] {{1,6},{8,10},{15,18}});
+    }
+
+    @Test
+    void test2() {
+        assertThat(intervalMerger.merge(new int[][] {{1,4},{4,5}}))
+                .isDeepEqualTo(new int[][] {{1,5}});
+    }
+
+    @Test
+    void test3() {
+        assertThat(intervalMerger.merge(new int[][] {{4,7},{1,4}}))
+                .isDeepEqualTo(new int[][] {{1,7}});
+    }
+
+    @Test
+    void test4() {
+        assertThat(intervalMerger.merge(new int[][] {{1,4}}))
+                .isDeepEqualTo(new int[][] {{1,4}});
+    }
+}
