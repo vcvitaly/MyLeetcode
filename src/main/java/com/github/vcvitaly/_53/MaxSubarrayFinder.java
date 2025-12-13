d9a725d520fba4d40240733cb4344655cbb4e374
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-12-13 10:44:44 +0100

    53 - WIP merging

--- src/main/java/com/github/vcvitaly/_53/MaxSubarrayFinder.java
+++ src/main/java/com/github/vcvitaly/_53/MaxSubarrayFinder.java
@@ -1,5 +1,8 @@
 package com.github.vcvitaly._53;
 
+import java.util.LinkedList;
+import java.util.List;
+
 // WIP
 public class MaxSubarrayFinder {
 
@@ -8,27 +11,58 @@ public class MaxSubarrayFinder {
             return nums[0];
         }
 
-        final int[] prefixSums = new int[nums.length];
-        prefixSums[0] = nums[0];
+
+        final List<Integer> merged = new LinkedList<>();
+
+        int max = Integer.MIN_VALUE;
+        boolean mergedPositive = nums[0] >= 0;
+        int mergedSum = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            prefixSums[i] = prefixSums[i-1] + nums[i];
-        }
-
-        int maxSubArray = Integer.MIN_VALUE;
-
-        int maxPrefixSumIndex = -1;
-        for (int i = 0; i < prefixSums.length; i++) {
-            if (prefixSums[i] > maxSubArray) {
-                maxSubArray = prefixSums[i];
-                maxPrefixSumIndex = i;
+            int cur = nums[i];
+            if (cur > max) {
+                max = cur;
+            }
+            boolean curPositive = cur >= 0;
+            if (mergedPositive == curPositive) {
+                mergedSum += cur;
+            } else {
+                merged.add(mergedSum);
+                mergedPositive = curPositive;
+                mergedSum = cur;
             }
         }
+        merged.add(mergedSum);
 
-        for (int i = 0; i < maxPrefixSumIndex; i++) {
-            int withoutPrefix = prefixSums[maxPrefixSumIndex] - prefixSums[i];
-            if (withoutPrefix > maxSubArray) {
-                maxSubArray = withoutPrefix;
+        if (merged.size() == 1) {
+            if (merged.getFirst() >= 0) {
+                return merged.getFirst();
             }
+            return max;
+        }
+        if (merged.size() == 2) {
+            return merged.stream().max(Integer::compareTo).get();
+        }
+
+        int maxSubArray = merged.getFirst() >= 0 ? merged.getFirst() : merged.get(1);
+        int curSubArray = maxSubArray;
+        int i = merged.getFirst() > 0 ? 1 : 2;
+        while (i < merged.size()) {
+            if (merged.get(i) < 0) {
+                if (Math.abs(merged.get(i)) < maxSubArray && i + 1 < merged.size() && maxSubArray + merged.get(i) + merged.get(i + 1) > maxSubArray) {
+                    curSubArray += merged.get(i) + merged.get(i + 1);
+                    i += 2;
+                }
+            } else {
+                if (merged.get(i) > maxSubArray) {
+                    maxSubArray = merged.get(i);
+                } else {
+                    curSubArray = merged.get(i);
+                }
+            }
+            if (curSubArray > maxSubArray) {
+                maxSubArray = curSubArray;
+            }
+            i++;
         }
 
         return maxSubArray;
