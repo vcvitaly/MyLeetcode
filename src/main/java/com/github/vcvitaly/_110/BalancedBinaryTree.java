f226371b51c6b13a803180bbcf7ee6bbea39a85b
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-12-14 12:37:03 +0100

    110 - solve

--- src/main/java/com/github/vcvitaly/_110/BalancedBinaryTree.java
+++ src/main/java/com/github/vcvitaly/_110/BalancedBinaryTree.java
@@ -14,7 +14,11 @@ public class BalancedBinaryTree {
             return true;
         }
 
-        return Math.abs(subTreeHeight(root.left) - subTreeHeight(root.right)) <= 1;
+        try {
+            return Math.abs(subTreeHeight(root.left) - subTreeHeight(root.right)) <= 1;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     private int subTreeHeight(TreeNode root) {
@@ -22,6 +26,13 @@ public class BalancedBinaryTree {
             return 0;
         }
 
-        return Math.max(subTreeHeight(root.left) + 1, subTreeHeight(root.right) + 1);
+        final int leftHeight = subTreeHeight(root.left) + 1;
+        final int rightHeight = subTreeHeight(root.right) + 1;
+        if (Math.abs(leftHeight - rightHeight) <= 1) {
+            return Math.max(leftHeight, rightHeight);
+        }
+        throw new UnbalancedTreeException();
     }
+
+    private static class UnbalancedTreeException extends RuntimeException {}
 }
