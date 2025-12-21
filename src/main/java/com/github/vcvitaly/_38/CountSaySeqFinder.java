44370e0b0a238818318b943636e1dd3df53de03e
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-12-21 19:01:03 +0100

    38 - draft

---
+++ src/main/java/com/github/vcvitaly/_38/CountSaySeqFinder.java
@@ -0,0 +1,9 @@
+package com.github.vcvitaly._38;
+
+// WIP
+public class CountSaySeqFinder {
+
+    public String countAndSay(int n) {
+        return null;
+    }
+}
---
+++ src/test/java/com/github/vcvitaly/_38/CountSaySeqFinderTest.java
@@ -0,0 +1,20 @@
+package com.github.vcvitaly._38;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CountSaySeqFinderTest {
+
+    private final CountSaySeqFinder finder = new CountSaySeqFinder();
+
+    @Test
+    void test1() {
+        assertThat(finder.countAndSay(4)).isEqualTo("1211");
+    }
+
+    @Test
+    void test2() {
+        assertThat(finder.countAndSay(1)).isEqualTo("1");
+    }
+}
