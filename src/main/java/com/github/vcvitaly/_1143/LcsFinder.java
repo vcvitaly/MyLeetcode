9e6b2778889109517be157b3f334a0384b4fdfe0
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-12-21 04:58:44 +0100

    1143 - WIP

--- src/main/java/com/github/vcvitaly/_1143/LcsFinder.java
+++ src/main/java/com/github/vcvitaly/_1143/LcsFinder.java
@@ -1,9 +1,52 @@
 package com.github.vcvitaly._1143;
 
+import java.util.*;
+
 // WIP
 public class LcsFinder {
 
     public int longestCommonSubsequence(String text1, String text2) {
-        return 0;
+        final String shorter = text1.length() > text2.length() ? text2 : text1;
+        final String longer = text1.length() > text2.length() ? text1 : text2;
+
+        Map<Character, List<Integer>> longerCharPositions = charPositions(longer);
+        int lcs = 0;
+        //
+
+
+        return -1;
+    }
+
+    private Map<Character, Integer> charCount(String s) {
+        final Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.merge(s.charAt(i), 1, Integer::sum);
+        }
+        return map;
+    }
+
+    private Map<Character, List<Integer>> charPositions(String s) {
+        final Map<Character, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.computeIfAbsent(s.charAt(i), key -> new ArrayList<>()).add(i);
+        }
+        return map;
+    }
+
+    private Queue<PositionContainer> positionContainerQueue(Map<Character, List<Integer>> charPositions) {
+        final List<PositionContainer> list = charPositions.entrySet().stream()
+                .map(e -> new PositionContainer(e.getValue(), e.getKey()))
+                .sorted()
+                .toList();
+        return new LinkedList<>(list);
+    }
+
+    private record PositionContainer(List<Integer> positions, char c) implements Comparable<PositionContainer> {
+        @Override
+        public int compareTo(PositionContainer o) {
+            return Comparator.comparingInt((PositionContainer pc) -> pc.positions().size())
+                    .thenComparing(PositionContainer::c)
+                    .compare(o, this);
+        }
     }
 }
--- src/test/java/com/github/vcvitaly/_1143/LcsFinderTest.java
+++ src/test/java/com/github/vcvitaly/_1143/LcsFinderTest.java
@@ -10,31 +10,61 @@ class LcsFinderTest {
 
     @Test
     void test1() {
-        assertThat(finder.longestCommonSubsequence("abcde", "ace")).isEqualTo(3);
+        assertThat(finder.longestCommonSubsequence("abcde", "ace")).isEqualTo(3); // ace
     }
 
     @Test
     void test2() {
         assertThat(finder.longestCommonSubsequence("abc", "abc")).isEqualTo(3);
-    }
+    } // ace
 
     @Test
     void test3() {
-        assertThat(finder.longestCommonSubsequence("abc", "def")).isEqualTo(0);
-    }
+        assertThat(finder.longestCommonSubsequence("abc", "def")).isZero();
+    } // no cs
 
     @Test
     void test4() {
-        assertThat(finder.longestCommonSubsequence("ace", "abcde")).isEqualTo(3);
+        assertThat(finder.longestCommonSubsequence("ace", "abcde")).isEqualTo(3); // ace
     }
 
     @Test
     void test5() {
-        assertThat(finder.longestCommonSubsequence("abcde", "aec")).isEqualTo(2);
+        assertThat(finder.longestCommonSubsequence("abcde", "aec")).isEqualTo(2); // ae or ac
     }
 
-    /*@Test
+    @Test
     void test6() {
-        assertThat(finder.longestCommonSubsequence("ababadadaddx", "ababadadaddy")).isEqualTo(2);
-    }*/
+        assertThat(finder.longestCommonSubsequence("ababadadaddx", "ababadadaddy")).isEqualTo(11); // ababadadadd
+    }
+
+    @Test
+    void test7() {
+        assertThat(finder.longestCommonSubsequence("ababadadaddx", "aaaadaddy")).isEqualTo(8); // aaaadadd
+    }
+
+    @Test
+    void test8() {
+        assertThat(finder.longestCommonSubsequence("aaabbb", "bbbaaa")).isEqualTo(3); // aaa or bbb
+    }
+
+    @Test
+    void test9() {
+        assertThat(finder.longestCommonSubsequence("aaaabbb", "bbbaaaa")).isEqualTo(4); // aaaa
+    }
+
+    @Test
+    void test10() {
+        assertThat(finder.longestCommonSubsequence("abccde", "edccba")).isEqualTo(2); // cc
+    }
+
+    @Test
+    void test11() {
+        assertThat(finder.longestCommonSubsequence("aaabbbb", "aaaabbb")).isEqualTo(6); // aaabbb
+    }
+
+    @Test
+    void test12() {
+        assertThat(finder.longestCommonSubsequence("abbacca", "aaabbcc")).isEqualTo(4); // bbcc
+    }
 }
