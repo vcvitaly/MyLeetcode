f2874ee0e19b8c2293ac30ecdab5a83201653a19
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-12-22 07:31:30 +0100

    45 - WIP

--- src/main/java/com/github/vcvitaly/_45/JumpGame2Calculator.java
+++ src/main/java/com/github/vcvitaly/_45/JumpGame2Calculator.java
@@ -3,6 +3,39 @@ package com.github.vcvitaly._45;
 public class JumpGame2Calculator {
 
     public int jump(int[] nums) {
-        return 0;
+        if (nums.length == 1) {
+            return 0;
+        }
+
+        if (nums.length == 2) {
+            return nums[0] >= 1 ? 1 : 0;
+        }
+
+        int[] distanceLeft = new int[nums.length];
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            distanceLeft[i] = Math.max(nums.length - i - nums[i], 0);
+        }
+
+        int jumps = 0;
+
+        int i = 0;
+        while (i < distanceLeft.length - 1) {
+            int j = i;
+            int minLeft = distanceLeft[j];
+            int minIndexLeft = j;
+            int boundary = Math.min(i + nums[i], distanceLeft.length - 1);
+            while (j < boundary) {
+                j++;
+                if (distanceLeft[j] + j - i < minLeft) {
+                    minLeft = distanceLeft[j];
+                    minIndexLeft = j;
+                }
+            }
+            i = minIndexLeft > i ? minIndexLeft : i + nums[i];
+            jumps++;
+        }
+
+        return jumps;
     }
 }
--- src/test/java/com/github/vcvitaly/_45/JumpGame2CalculatorTest.java
+++ src/test/java/com/github/vcvitaly/_45/JumpGame2CalculatorTest.java
@@ -22,4 +22,34 @@ class JumpGame2CalculatorTest {
     void test3() {
         assertThat(calculator.jump(new int[]{2,3,0,1,4})).isEqualTo(2);
     }
+
+    @Test
+    void test4() {
+        assertThat(calculator.jump(new int[]{1,1,1,1,1})).isEqualTo(4);
+    }
+
+    @Test
+    void test5() {
+        assertThat(calculator.jump(new int[]{4,6,8,1,1,1,1,1,1,1,1})).isEqualTo(2);
+    }
+
+    @Test
+    void test6() {
+        assertThat(calculator.jump(new int[]{3,3,3,3,3,3,3,3,3})).isEqualTo(3);
+    }
+
+    @Test
+    void test7() {
+        assertThat(calculator.jump(new int[]{0})).isZero();
+    }
+
+    @Test
+    void test8() {
+        assertThat(calculator.jump(new int[]{1,1})).isEqualTo(1);
+    }
+
+    @Test
+    void test9() {
+        assertThat(calculator.jump(new int[]{5,4,3,2,1,2,1,1})).isEqualTo(2);
+    }
 }
