44e5cfd8eda4c841f460df08a3ed1b58e949bcbd
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-12-31 07:19:35 +0100

    124 - WIP

--- src/main/java/com/github/vcvitaly/_124/BinaryTreeMaxPathSumFinder.java
+++ src/main/java/com/github/vcvitaly/_124/BinaryTreeMaxPathSumFinder.java
@@ -2,9 +2,89 @@ package com.github.vcvitaly._124;
 
 import com.github.vcvitaly.common.TreeNode;
 
+import java.util.stream.Stream;
+
+// WIP
 public class BinaryTreeMaxPathSumFinder {
 
     public int maxPathSum(TreeNode root) {
-        return 0;
+        if (root == null) {
+            return 0;
+        }
+
+        if (isLeaf(root)) {
+            return root.val;
+        }
+
+        return findSumPair(root).bestPathSum;
     }
+
+    private Result findSumPair(TreeNode root) {
+        if (root == null) {
+            return Result.ofSingleNode(null);
+        }
+
+        if (isLeaf(root)) {
+            return Result.ofSingleNode(root);
+        }
+
+        final Result leftResult = findSumPair(root.left);
+        final Result rightResult = findSumPair(root.right);
+
+        final SubPath newSubPath = leftResult.bestSubPath.sum >= rightResult.bestSubPath.sum ?
+                join(leftResult.bestSubPath, root) : join(rightResult.bestSubPath, root);
+
+        if (equals(leftResult.bestSubPath.subRoot, root.left) && equals(rightResult.bestSubPath.subRoot, root.right)) {
+            return new Result(
+                    newSubPath,
+                    Stream.of(
+                            newSubPath.sum,
+                            leftResult.bestSubPath.sum + rightResult.bestSubPath.sum + root.val,
+                            leftResult.bestPathSum,
+                            rightResult.bestPathSum,
+                            root.val
+                    ).max(Integer::compareTo).orElseThrow()
+            );
+        }
+
+        final int newBestPathSum = Math.max(newSubPath.sum, Math.max(leftResult.bestPathSum, rightResult.bestPathSum));
+        return new Result(newSubPath, newBestPathSum);
+    }
+
+    private boolean isLeaf(TreeNode root) {
+        return root.left == null && root.right == null;
+    }
+
+    private boolean equals(TreeNode root1, TreeNode root2) {
+        return System.identityHashCode(root1) == System.identityHashCode(root2);
+    }
+
+    private SubPath join(SubPath subPath, TreeNode root) {
+        return new SubPath(subPath.sum + root.val, root);
+    }
+
+    private record SubPath(int sum, TreeNode subRoot) {}
+
+    private record Result(SubPath bestSubPath, int bestPathSum) {
+
+        private static Result ofSingleNode(TreeNode root) {
+            int val = root == null ? Integer.MIN_VALUE / 10 : root.val;
+            return new Result(
+                    new SubPath(val, root),
+                    val
+            );
+        }
+    }
+
+    /*if (equals(leftResult.bestSubPath.subRoot, root.left) && equals(rightResult.bestSubPath.subRoot, root.right)) {
+        return null;
+    } else if (equals(leftResult.bestSubPath.subRoot, root.left)) {
+        final SubPath newSubPath = new SubPath(leftResult.bestSubPath.sum + root.val, root);
+        return new Result(newSubPath, newSubPath.sum > leftResult.bestPathSum ? newSubPath.sum : leftResult.bestPathSum);
+    } else if (equals(rightResult.bestSubPath.subRoot, root.right)) {
+        return null;
+    } else {
+        throw new IllegalStateException("At val " + root.val);
+    }
+    return new Result(null, Math.max(leftResult.bestPathSum, rightResult.bestPathSum));*/
 }
--- src/test/java/com/github/vcvitaly/_124/BinaryTreeMaxPathSumFinderTest.java
+++ src/test/java/com/github/vcvitaly/_124/BinaryTreeMaxPathSumFinderTest.java
@@ -21,4 +21,22 @@ class BinaryTreeMaxPathSumFinderTest {
         final TreeNode root = TreeUtil.buildTreeNode(new Integer[]{-10,9,20,null,null,15,7});
         assertThat(finder.maxPathSum(root)).isEqualTo(42);
     }
+
+    @Test
+    void test3() {
+        final TreeNode root = TreeUtil.buildTreeNode(new Integer[]{-2,-1});
+        assertThat(finder.maxPathSum(root)).isEqualTo(-1);
+    }
+
+    @Test
+    void test4() {
+        final TreeNode root = TreeUtil.buildTreeNode(new Integer[]{2,-1});
+        assertThat(finder.maxPathSum(root)).isEqualTo(2);
+    }
+
+    @Test
+    void test5() {
+        final TreeNode root = TreeUtil.buildTreeNode(new Integer[]{-1,5,null,4,null,null,2,-4});
+        assertThat(finder.maxPathSum(root)).isEqualTo(11);
+    }
 }
