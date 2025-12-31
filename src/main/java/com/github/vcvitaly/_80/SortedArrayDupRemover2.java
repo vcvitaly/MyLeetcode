311e733b4979ee76301f4231384a1399fa612413
Vitalii Chura <vitalii.chura@pitchbook.com>
2025-12-31 05:15:13 +0100

    80 - WIP

--- src/main/java/com/github/vcvitaly/_80/SortedArrayDupRemover2.java
+++ src/main/java/com/github/vcvitaly/_80/SortedArrayDupRemover2.java
@@ -3,6 +3,20 @@ package com.github.vcvitaly._80;
 public class SortedArrayDupRemover2 {
 
     public int removeDuplicates(int[] nums) {
+        if (nums.length == 1) {
+            return 1;
+        }
+
+        int k = 0;
+        boolean hasPair = false;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[k]) {
+
+            } else {
+
+            }
+        }
+
         return 0;
     }
 }
