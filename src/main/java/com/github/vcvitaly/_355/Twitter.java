cd143bf20fae3e6bd34fb505e38cf987e456f88f
Vitalii Chura <vitalii.chura@pitchbook.com>
2026-02-01 10:39:18 +0100

    355 - draft

---
+++ src/main/java/com/github/vcvitaly/_355/Twitter.java
@@ -0,0 +1,26 @@
+package com.github.vcvitaly._355;
+
+import java.util.List;
+
+public class Twitter {
+
+    public Twitter() {
+
+    }
+
+    public void postTweet(int userId, int tweetId) {
+
+    }
+
+    public List<Integer> getNewsFeed(int userId) {
+        return null;
+    }
+
+    public void follow(int followerId, int followeeId) {
+
+    }
+
+    public void unfollow(int followerId, int followeeId) {
+
+    }
+}
