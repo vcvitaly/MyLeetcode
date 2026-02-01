4486c6bcdfada495ae93e5c700f6119361273989
Vitalii Chura <vitalii.chura@pitchbook.com>
2026-02-01 08:31:21 +0100

    353 - draft

---
+++ src/main/java/com/github/vcvitaly/_353/SnakeGame.java
@@ -0,0 +1,12 @@
+package com.github.vcvitaly._353;
+
+public class SnakeGame {
+
+    public SnakeGame(int width, int height, int[][] food) {
+
+    }
+
+    public int move(String direction) {
+        return 0;
+    }
+}
