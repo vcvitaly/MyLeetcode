71e0ba8c6df64ebd889bb97540a0f534c6ce7bff
Vitalii Chura <vitalii.chura@pitchbook.com>
2026-02-03 18:52:43 +0100

    355 - draft 1st approach

--- src/main/java/com/github/vcvitaly/_355/Twitter.java
+++ src/main/java/com/github/vcvitaly/_355/Twitter.java
@@ -1,26 +1,58 @@
 package com.github.vcvitaly._355;
 
-import java.util.List;
+import java.util.*;
 
+// 1. Fast write, slow reads with merge k sorted lists naive approach
+// 2. Maintain a priority Q of sorted lists for each user, slower writes
+// 3. Fanout approach with maintain top 10 tweets per user
 public class Twitter {
 
-    public Twitter() {
+    private static final int TOP_K_COUNT = 10;
+    private final Map<Integer, Set<Integer>> followeesByUser = new HashMap<>();
+    private final Map<Integer, List<Integer>> tweetsByUser = new HashMap<>();
 
-    }
+    public Twitter() {}
 
     public void postTweet(int userId, int tweetId) {
-
+        tweetsByUser.computeIfAbsent(userId, k -> new ArrayList<>()).add(tweetId);
     }
 
     public List<Integer> getNewsFeed(int userId) {
-        return null;
+        final Set<Integer> followees = followeesByUser.get(userId);
+        if (followees == null || followees.isEmpty()) {
+            return List.of();
+        }
+        final List<Integer> resultTweets = new ArrayList<>(TOP_K_COUNT);
+        final Map<Integer, Integer> positionByUser = new HashMap<>();
+        for (int i = 0; i < TOP_K_COUNT; i++) {
+            int maxTweet = -1;
+            int maxTweetAuthorId = 0;
+            for (Integer followee : followees) {
+                Integer nextIndexByFollowee = positionByUser.computeIfAbsent(followee, k -> tweetsByUser.get(k).size() - 1);
+                Integer nextMaxTweetByThisFollowee = tweetsByUser.get(followee).get(nextIndexByFollowee);
+                if (nextMaxTweetByThisFollowee > maxTweet) {
+                    maxTweet = nextMaxTweetByThisFollowee;
+                    maxTweetAuthorId = followee;
+                }
+            }
+            resultTweets.add(maxTweet);
+            positionByUser.compute(maxTweetAuthorId, (k, nextIndexByFollowee) -> nextIndexByFollowee - 1);
+        }
+        return resultTweets;
     }
 
     public void follow(int followerId, int followeeId) {
-
+        if (followerId == followeeId) {
+            throw new IllegalArgumentException("User cannot follow himself");
+        }
+        followeesByUser.computeIfAbsent(followerId, k -> new HashSet<>()).add(followeeId);
     }
 
     public void unfollow(int followerId, int followeeId) {
-
+        if (followerId == followeeId) {
+            throw new IllegalArgumentException("User cannot unfollow himself");
+        }
+        final Set<Integer> followees = this.followeesByUser.get(followerId);
+        followees.remove(followeeId);
     }
 }
---
+++ src/test/java/com/github/vcvitaly/_355/TwitterTest.java
@@ -0,0 +1,44 @@
+package com.github.vcvitaly._355;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TwitterTest {
+
+    private final Twitter twitter = new Twitter();
+
+    @Test
+    void test1() {
+        runTestHarness(
+                new String[] {"postTweet", "getNewsFeed", "follow", "postTweet", "getNewsFeed", "unfollow", "getNewsFeed"},
+                new int[][] {{1, 5}, {1}, {1, 2}, {2, 6}, {1}, {1, 2}, {1}},
+                Arrays.asList(null, List.of(5), null, null, List.of(6, 5), null, List.of(5))
+        );
+    }
+
+    private void runTestHarness(String[] actions, int[][] arguments, List<List<Integer>> expectedResults) {
+        for (int i = 0; i < actions.length; i++) {
+            if (actions[i].equals("postTweet")) {
+                twitter.postTweet(arguments[i][0], arguments[i][1]);
+            } else if (actions[i].equals("getNewsFeed")) {
+                final List<Integer> newsFeed = twitter.getNewsFeed(arguments[i][0]);
+                final List<Integer> expected = expectedResults.get(i);
+                int finalI = i;
+                assertThat(newsFeed)
+                        .withFailMessage(() -> "Expecting getNewsFeed to return %s at step %d but it returned %s"
+                                .formatted(expected, finalI, newsFeed))
+                        .containsExactlyElementsOf(expected);
+            } else if (actions[i].equals("follow")) {
+                twitter.follow(arguments[i][0], arguments[i][1]);
+            } else if (actions[i].equals("unfollow")) {
+                twitter.unfollow(arguments[i][0], arguments[i][1]);
+            } else {
+                throw new IllegalArgumentException("Unknown action: " + actions[i]);
+            }
+        }
+    }
+}
