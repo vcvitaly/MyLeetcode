0d95c99cee68149f172f3e29b167144e5c31d364
Vitalii Chura <vitalii.chura@pitchbook.com>
2026-02-08 17:32:22 +0100

    362 - draft

---
+++ src/main/java/com/github/vcvitaly/_362/HitCounter.java
@@ -0,0 +1,16 @@
+package com.github.vcvitaly._362;
+
+public class HitCounter {
+
+    public HitCounter() {
+
+    }
+
+    public void hit(int timestamp) {
+
+    }
+
+    public int getHits(int timestamp) {
+        return 0;
+    }
+}
