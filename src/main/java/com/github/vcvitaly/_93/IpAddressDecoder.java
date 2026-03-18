df73f81c95e3ec19371feafd968708513ebea604
Vitalii Chura <vitalii.chura@pitchbook.com>
2026-03-18 18:16:34 +0100

    93 - solve slowly

--- src/main/java/com/github/vcvitaly/_93/IpAddressDecoder.java
+++ src/main/java/com/github/vcvitaly/_93/IpAddressDecoder.java
@@ -1,10 +1,61 @@
 package com.github.vcvitaly._93;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class IpAddressDecoder {
 
     public List<String> restoreIpAddresses(String s) {
-        return null;
+        if (s.length() < 4 || s.length() > 12) {
+            return List.of();
+        }
+
+        return restoreNGroups(s, 0, 4);
+    }
+
+    private List<String> restoreNGroups(String s, int startIndex, int n) {
+        if (n == 1) {
+            if (s.length() - startIndex <= 3) {
+                final String lastOctet = s.substring(startIndex);
+                if (!validOctet(lastOctet)) {
+                    return List.of();
+                }
+                return List.of(lastOctet);
+            }
+            return List.of();
+        }
+
+
+        final List<String> result = new ArrayList<>();
+        for (int i = 1; i <= 3 && s.length() - (startIndex + i - 1) >= n; i++) {
+            final String currentGroup;
+            try {
+                currentGroup = s.substring(startIndex, startIndex + i);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            if (!validOctet(currentGroup)) {
+                break;
+            }
+            final List<String> restoredNGroups = restoreNGroups(s, startIndex + i, n - 1);
+            if (!restoredNGroups.isEmpty()) {
+                result.addAll(
+                        restoredNGroups.stream()
+                                .map(nGroup -> currentGroup + "." + nGroup)
+                                .toList()
+                );
+            }
+        }
+
+        return result;
+    }
+
+    private boolean validOctet(String octet) {
+        int asInt = Integer.parseInt(octet);
+        return asInt <= 255 && !isZeroPadded(octet);
+    }
+
+    private boolean isZeroPadded(String currentGroup) {
+        return currentGroup.length() > 1 && currentGroup.charAt(0) == '0';
     }
 }
--- src/test/java/com/github/vcvitaly/_93/IpAddressDecoderTest.java
+++ src/test/java/com/github/vcvitaly/_93/IpAddressDecoderTest.java
@@ -27,4 +27,10 @@ class IpAddressDecoderTest {
         assertThat(decoder.restoreIpAddresses("101023"))
                 .containsExactlyInAnyOrderElementsOf(List.of("1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"));
     }
+
+    @Test
+    void test4() {
+        assertThat(decoder.restoreIpAddresses("172162541"))
+                .containsExactlyInAnyOrderElementsOf(List.of("17.216.25.41","17.216.254.1","172.16.25.41","172.16.254.1","172.162.5.41","172.162.54.1"));
+    }
 }
