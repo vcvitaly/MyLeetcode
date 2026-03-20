6f07983218582db456a56da70f74928b47438cdf
Vitalii Chura <vitalii.chura@pitchbook.com>
2026-03-20 18:57:36 +0100

    97 - WIP

--- src/main/java/com/github/vcvitaly/_97/StringInterleavingDeterminator.java
+++ src/main/java/com/github/vcvitaly/_97/StringInterleavingDeterminator.java
@@ -1,8 +1,38 @@
 package com.github.vcvitaly._97;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
+// WIP
 public class StringInterleavingDeterminator {
 
     public boolean isInterleave(String s1, String s2, String s3) {
-        return false;
+        final Map<Character, Queue<Integer>> positions = new HashMap<>();
+        for (int i = 0; i < s3.length(); i++) {
+            final char c = s3.charAt(i);
+            positions.computeIfAbsent(c, k -> new LinkedList<>()).add(i);
+        }
+
+        for (int i = 0; i < s1.length(); i++) {
+            final char c = s1.charAt(i);
+            final Queue<Integer> q = positions.get(c);
+            if (q != null && !q.isEmpty()) {
+                q.poll();
+            } else {
+                return false;
+            }
+        }
+
+        final String leftover = positions.entrySet().stream()
+                .flatMap(e -> e.getValue().stream()
+                        .map(pos -> new LetterPosition(e.getKey(), pos))
+                ).sorted(Comparator.comparing(LetterPosition::position))
+                .map(LetterPosition::c)
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+
+        return leftover.equals(s2);
     }
+
+    private record LetterPosition(char c, int position) {}
 }
--- src/test/java/com/github/vcvitaly/_97/StringInterleavingDeterminatorTest.java
+++ src/test/java/com/github/vcvitaly/_97/StringInterleavingDeterminatorTest.java
@@ -12,14 +12,26 @@ class StringInterleavingDeterminatorTest {
     void test1() {
         assertThat(determinator.isInterleave("aabcc", "dbbca", "aadbbcbcac")).isTrue();
     }
+    // dbbac
 
     @Test
     void test2() {
         assertThat(determinator.isInterleave("aabcc", "dbbca", "aadbbbaccc")).isFalse();
     }
+    // dbbac
 
     @Test
     void test3() {
         assertThat(determinator.isInterleave("", "", "")).isTrue();
+    }
+
+    @Test
+    void test4() {
+        assertThat(determinator.isInterleave("aaa", "bbb", "ababab")).isTrue();
+    }
+
+    @Test
+    void test5() {
+        assertThat(determinator.isInterleave("aaa", "bbb", "bababa")).isTrue();
     }
 }
