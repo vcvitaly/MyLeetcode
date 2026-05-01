d5b53fd6a5b52ead7e4cec09efc282de26f9372d
Vitalii Chura <vitalii.chura@pitchbook.com>
2026-05-01 19:15:35 +0200

    136 - rework test

--- src/test/java/com/github/vcvitaly/_136/SingleNumberTest.java
+++ src/test/java/com/github/vcvitaly/_136/SingleNumberTest.java
@@ -1,43 +1,28 @@
 package com.github.vcvitaly._136;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 
 class SingleNumberTest {
 
-    @ParameterizedTest
-    @MethodSource("params")
-    void findsTheNumberWhichOccursOnce(Param param) {
-        System.out.println(param);
-
+    @Test
+    void test1() {
         Assertions.assertThat(
-                SingleNumber.singleNumber(param.nums)
-        ).isEqualTo(param.number);
+                SingleNumber.singleNumber(new int[] {2, 2, 1})
+        ).isEqualTo(1);
     }
 
-    static Stream<Param> params() {
-        return Stream.of(
-                Param.of(
-                        new int[] {2,2,1}, 1
-                ),
-                Param.of(
-                        new int[] {4,1,2,1,2}, 4
-                ),
-                Param.of(
-                        new int[] {1}, 1
-                )
-        );
+    @Test
+    void test2() {
+        Assertions.assertThat(
+                SingleNumber.singleNumber(new int[] {4, 1, 2, 1, 2})
+        ).isEqualTo(4);
     }
 
-    @Data
-    @AllArgsConstructor(staticName = "of")
-    private static class Param {
-        public int[] nums;
-        private int number;
+    @Test
+    void test3() {
+        Assertions.assertThat(
+                SingleNumber.singleNumber(new int[] {1})
+        ).isEqualTo(1);
     }
 }
