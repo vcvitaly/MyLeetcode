956ab98b48807e176f35d4eb4ff418f590ecb098
Vitalii Chura <vitalii.chura@pitchbook.com>
2026-05-01 19:31:07 +0200

    228 - draft

---
+++ src/main/java/com/github/vcvitaly/_228/SummaryRangesFinder.java
@@ -0,0 +1,10 @@
+package com.github.vcvitaly._228;
+
+import java.util.List;
+
+public class SummaryRangesFinder {
+
+    public List<String> summaryRanges(int[] nums) {
+        return null;
+    }
+}
